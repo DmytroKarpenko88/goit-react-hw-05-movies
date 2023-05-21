c2bec45798e327e37c3affdcff93e85f43a9256d
Dmytro Karpenko <kardmitriy@gmail.com>
2023-05-22 00:41:50 +0300

    update

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,10 @@
-import { Grid } from 'components';
+import { Grid, GridItem } from 'components';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieCredits } from 'services/api';
+
+import photo_Pattern from '../../components/Images/person-png-icon-1.png';
+import { IMG_URL } from 'services/variables';
 
 const Cast = () => {
   const { query } = useParams();
@@ -20,16 +23,18 @@ const Cast = () => {
       <Grid>
         {casts.map(({ id, name, profile_path, character }) => {
           return (
-            <li key={id}>
+            <GridItem key={id}>
               <div>
                 <img
-                  src={`https://image.tmdb.org/t/p/original${profile_path}`}
+                  src={
+                    profile_path ? `${IMG_URL}${profile_path}` : photo_Pattern
+                  }
                   alt={name}
                 />
                 <p>{name}</p>
                 <p>{character}</p>
               </div>
-            </li>
+            </GridItem>
           );
         })}
       </Grid>
