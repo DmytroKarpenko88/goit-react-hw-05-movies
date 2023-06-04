af913bf86a13f91edd42f9172cede382df5fbe9b
Dmytro Karpenko <kardmitriy@gmail.com>
2023-06-04 07:53:18 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,7 +22,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="*" element={<PageNotFound />}></Route>
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </div>
